2b53f257bc773a1f23a7fea69a4b7f4b4c2a14f6
Nadin-T <nadezhda.tarasova8@ya.ru>
2024-10-28 11:41:22 +0300

    added 'module_1_6.py'

---
+++ module_1/module_1_6.py
@@ -0,0 +1,9 @@
+"""variables"""
+
+home_work_count = 12
+home_work_hours = 1.5
+course_name = 'Python'
+task_time = home_work_hours / home_work_count
+
+print(f'Курс: {course_name}, всего задач: {home_work_count}, '
+      f'затрачено часов: {home_work_hours}, среднее время выполнения {task_time} часа.')
