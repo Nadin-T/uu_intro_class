88d65a84b1b9e88e6e02b5575b74e45700c7a90d
Nadin-T <nadezhda.tarasova8@ya.ru>
2024-11-15 13:54:03 +0300

    added 'module_5_4', changed 'module_5_3'

--- module_5/module_5_3.py
+++ module_5/module_5_3.py
@@ -1,4 +1,13 @@
 class House():
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        house = super(House, cls).__new__(cls)
+        if args and args[0] not in cls.houses_history:
+            cls.houses_history.append(args[0])
+            return house
+
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
@@ -44,27 +53,30 @@ class House():
     def __iadd__(self, value): # +=
         return self.__add__(value)
 
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории.')
 
-h1 = House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация', 20)
+if __name__ == '__main__':
+    h1 = House('ЖК Эльбрус', 10)
+    h2 = House('ЖК Акация', 20)
 
-print(h1)
-print(h2)
+    print(h1)
+    print(h2)
 
-print(h1 == h2) # __eq__
+    print(h1 == h2) # __eq__
 
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
+    h1 = h1 + 10 # __add__
+    print(h1)
+    print(h1 == h2)
 
-h1 += 10 # __iadd__
-print(h1)
+    h1 += 10 # __iadd__
+    print(h1)
 
-h2 = 10 + h2 # __radd__
-print(h2)
+    h2 = 10 + h2 # __radd__
+    print(h2)
 
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+    print(h1 > h2) # __gt__
+    print(h1 >= h2) # __ge__
+    print(h1 < h2) # __lt__
+    print(h1 <= h2) # __le__
+    print(h1 != h2) # __ne__
