303f3f3f79499c26d9047aa1fc04b5e617d57eca
Nadin-T <nadezhda.tarasova8@ya.ru>
2024-11-15 15:02:15 +0300

    changed 'registration_system.py'

--- module_5/registration_system.py
+++ module_5/registration_system.py
@@ -1,3 +1,6 @@
+from random import choice
+
+
 class Database:
     def __init__(self):
         self.data = {}
@@ -5,16 +8,55 @@ class Database:
     def add_user(self, username, password):
         self.data[username] = password
 
+
 class User:
     """
     Класс пользователя, содержащий атрибуты: логин, пароль
     """
     def __init__(self, username, password, password_confirm):
         self.username = username
-        if  password == password_confirm:
+        if  password == password_confirm and password_validation(password):
             self.password = password
+        else:
+            print('Пароль неверен')
+            self.password = None
+
+def password_validation(password):
+    password_valid = False
+    if len(password) >= 8:
+        int_count = str_upper_count = 0
+        for char in password:
+            if char.isdigit():
+                int_count += 1
+            elif char == char.upper():
+                str_upper_count += 1
+        if int_count >= 1 and str_upper_count >= 1:
+            password_valid = True
+    else:
+        print('Неверный размер пароля. Пароль должен содержать не менее 8 символов, '
+              'иметь минимум 1 заглавную букву и минимум 1 цифру.')
+    return password_valid
+
 
 if __name__ == '__main__':
     database = Database()
-    user = User(input('Введите логин: '), input('Введите пароль: '), input('Повторите пароль: '))
-    database.add_user(user.username, user.password)
+    print('Приветствую!')
+    while True:
+        choice = input('Выберите действие: \n1 - Вход в систему\n2 - Регистрация\n0 - Выход\n')
+        # if choice == '1':
+        #     pass
+        if choice == '2':
+            user = User(
+                input('Введите логин: '),
+                password := input('Введите пароль: '),
+                password2 := input('Повторите пароль: ')
+            )
+            if password != password2:
+                exit()
+            if user.username is not None and user.password is not None:
+                database.add_user(user.username, user.password)
+                print(database.data)
+            else:
+                print('Регистрация не удалась')
+        if choice == '0':
+            break
