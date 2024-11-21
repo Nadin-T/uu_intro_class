9e49e4aa729ffad8e3faf782cf9704d889df7c51
Nadin-T <nadezhda.tarasova8@ya.ru>
2024-11-21 14:02:55 +0300

    added 'module_6_1.py'

---
+++ module_6/module_6_1.py
@@ -0,0 +1,60 @@
+class Animal:
+    """Класс животное"""
+    alive = True #живой
+    fed = False #накормленный
+
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}.')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}.')
+            self.alive = False
+
+class Plant:
+    """Класс растение"""
+    def __init__(self, name):
+        self.name = name
+        self.edible = False  # съедобность
+
+
+class Mammal(Animal):
+    """Класс млекопитающие"""
+    pass
+
+
+class Predator(Animal):
+    """Класс хищник"""
+    pass
+
+
+class Flower(Plant):
+    """Класс растения"""
+    pass
+
+
+class Fruit(Plant):
+    """Класс фрукты"""
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
+
+
+if __name__ == '__main__':
+    a1 = Predator('Волк с Уолл-Стрит')
+    a2 = Mammal('Хатико')
+    p1 = Flower('Цветик семицветик')
+    p2 = Fruit('Заводной апельсин')
+
+    print(a1.name)
+    print(p1.name)
+    print(a1.alive)
+    print(a2.fed)
+
+    a1.eat(p1)
+    a2.eat(p2)
+    print(a1.alive)
+    print(a2.fed)
